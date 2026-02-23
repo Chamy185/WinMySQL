ca412400e7acfe03650695421d4cd3437cd8d862
rodes115 <rodesreyes115@gmail.com>
2026-02-23 15:48:54 -0600

    Agregar archivos de proyecto.

---
+++ WinMySQL/Clases/Datos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace WinMySQL.Clases
+{
+    internal class Datos
+    {
+        String cadenaConexion = "server=localhost;user=luis;pwd=joseluis";
+        MySqlConnection conexion;
+
+        private void conectar()
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+        private void Desconectar()
+        {
+            try
+            {
+                if(conexion != null)
+                {
+                    conexion.Close();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+
+    }
+}
